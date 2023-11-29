8b4c0a7e7b7a6c92cf2cce5c209160169f1ab97b
philcifone <phil@philcifone.com>
2023-11-29 11:50:00 -0500

    task update with more datapoints, need to make daily contstants a single input

--- tasks.py
+++ tasks.py
@@ -2,30 +2,44 @@ import csv
 import datetime
 
 def log_task_completion():
-    section = input("Enter the section (e.g., Boys Bathroom): ")
-    task_number = int(input("Enter the task number: "))
-    start_time = datetime.datetime.now()
-    
-    # Simulate cleaning task
-    input("Press Enter when you've completed the cleaning task...")
-    
-    end_time = datetime.datetime.now()
-    duration = end_time - start_time
-    
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%s')
+    section = input("Enter the section/task (e.g., boysBath, upHallDry, gym1Wet): ")
+    taskNumber = int(input("Enter the task number: "))
+    date = input("Enter the date completed in YYYY-MM-DD format: ")
+    size = int(input("Enter the square footage of the section cleaned: "))
+    duration = input("Enter time to complete task in Hours:Minutes:Seconds format: ")
+    dirtLevel = int(input("Enter 'dirtyness' level from 1-10, 10 being the dirtiest: "))
+    gloves = int(input("How many pairs of gloves did you go through?: "))
+    weatherTemp = int(input("What was the temperature outside in Fahrenheit?: "))
+    weatherCond = input("What were weather conditions like? (e.g. sunny, cloudy, rain, snow): ")
+    moonPhase = input("By illumination percentage, what is the moon phase?: ")
     
     with open('task_completion_log.csv', 'a', newline='') as csvfile:
-        fieldnames = ['Timestamp', 'Section', 'Task Number', 'Start Time', 'End Time', 'Duration']
+        fieldnames = [
+            'Date', 
+            'Section', 
+            'Task Number', 
+            'Size', 
+            'Duration', 
+            'Dirt Level', 
+            'Gloves', 
+            'Weather Temp', 
+            'Weather Conditions', 
+            'Moon Phase',
+            ]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if csvfile.tell() == 0:  # Write header only if the file is empty
             writer.writeheader()
         writer.writerow({
-            'Timestamp': timestamp,
+            'Date': date,
             'Section': section,
-            'Task Number': task_number,
-            'Start Time': start_time.strftime('%H:%M:%S'),
-            'End Time': end_time.strftime('%H:%M:%S'),
-            'Duration': str(duration)
+            'Task Number': taskNumber,
+            'Size': size,
+            'Duration': str(duration),
+            'Dirt Level': dirtLevel,
+            'Gloves': gloves,
+            'Weather Temp': weatherTemp,
+            'Weather Conditions': weatherCond,
+            'Moon Phase': moonPhase,
         })
 
 # Example usage
