e4db5b529f1be87d78db8f164226106f05ac4359
philcifone <phil@philcifone.com>
2023-12-01 02:29:59 -0500

    tweaks, formatting, better graphs

--- generalAnalysis.py
+++ generalAnalysis.py
@@ -12,42 +12,37 @@ df['Date'] = pd.to_datetime(df['Date'])
 df['Duration'] = pd.to_timedelta(df['Duration'])
 
 # Get the order of sections based on the minimum 'Task Number'
-section_order = df.groupby('Section')['Task Number'].min().sort_values().index
+#section_order = df.groupby('Section')['Task Number'].min().sort_values().index
 
 # Plotting examples
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
 fig.suptitle('Daily Task Completion Analysis')
 
-# Plot 1: Task completion count per day
-df['Date'].value_counts().sort_index().plot(ax=axes[0, 0], kind='bar', color='skyblue')
-axes[0, 0].set_title('Task Completion Count per Day')
+# Plot 1: Total gloves used per day
+df.groupby(df['Date'].dt.strftime('%Y-%m-%d'))['Gloves'].sum().plot(kind='bar', color='purple', ax=axes[0, 0])
 axes[0, 0].set_xlabel('Date')
-axes[0, 0].set_ylabel('Count')
+axes[0, 0].set_ylabel('Total Gloves Used')
 
-# Plot 2: Average duration of tasks per day
-df.groupby('Date')['Duration'].mean().dt.total_seconds().div(60).plot(ax=axes[1, 1], kind='line', marker='o', color='orange')
-axes[1, 1].set_title('Average Duration of Tasks per Day (minutes)')
-axes[1, 1].set_xlabel('Date')
-axes[1, 1].set_ylabel('Average Duration')
 
-#Plot 2
-#df['Duration_minutes'] = df['Duration'].dt.total_seconds() / 60
-#df.plot(x='Section', y='Duration_minutes', kind='bar', ax=axes[0, 0], color='skyblue', legend=False)
-#axes[0, 0].set_title(f'Duration of Tasks per Day - {csv_date}')
-#axes[0, 0].set_xlabel('Section')
-#axes[0, 0].set_ylabel('Duration (minutes)')
+# Plot 2: Average duration of tasks per section
+df['Duration'] = pd.to_timedelta(df['Duration'])
+df.groupby('Section')['Duration'].mean().dt.total_seconds().div(60).plot(ax=axes[1, 1], kind='bar', color='purple')
+axes[1, 1].set_title('Average Duration of Tasks per Section')
+axes[1, 1].set_xlabel('Section')
+axes[1, 1].set_ylabel('Time in minutes')
 
 # Plot 3: Dirt level distribution
-df['Dirt Level'].plot(ax=axes[1, 0], kind='hist', bins=10, color='green', edgecolor='black')
-axes[1, 0].set_title('Distribution of Dirt Levels')
-axes[1, 0].set_xlabel('Dirt Level')
-axes[1, 0].set_ylabel('Frequency')
+#df['Dirt Level'].plot(ax=axes[1, 0], kind='hist', bins=10, color='green', edgecolor='black')
+#axes[1, 0].set_title('Distribution of Dirt Levels')
+#axes[1, 0].set_xlabel('Dirt Level')
+#axes[1, 0].set_ylabel('Frequency')
 
 # Plot 3: Dirt levels per task
-#df.plot(x='Section', y='Dirt Level', kind='bar', ax=axes[1, 0], color='green', legend=False)
-#axes[1, 0].set_title(f'Dirt Levels per Task - {csv_date}')
-#axes[1, 0].set_xlabel('Section')
-#axes[1, 0].set_ylabel('Dirt Level')
+df.groupby('Section')['Dirt Level'].mean().plot(ax=axes[1, 0], kind='bar', color='green')
+axes[1, 0].set_title('Dirt Levels per Task')
+axes[1, 0].set_xlabel('Section')
+axes[1, 0].set_ylabel('Dirt Level')
+#axes[1, 0].tick_params(axis='x', rotation=90)
 
 # Plot 5: Duration per square foot per section
 # Calculate duration per square foot
@@ -56,11 +51,10 @@ df['Duration_per_sqft'] = df['Duration'] / df['Size']
 # Convert Timedelta to seconds
 df['Duration_per_sqft'] = df['Duration_per_sqft'].dt.total_seconds()
 # Actual Plot
-df.groupby('Section')['Duration_per_sqft'].mean().loc[section_order].plot(kind='bar', ax=axes[0, 1], color='orange', legend=False)
+df.groupby('Section')['Duration_per_sqft'].mean().plot(kind='bar', ax=axes[0, 1], color='orange', legend=False)
 axes[0, 1].set_title(f'Average Duration per Square Foot per Section')
 axes[0, 1].set_xlabel('Section')
-axes[0, 1].set_ylabel('Average Duration per Square Foot')
-#axes[0, 1].tick_params(axis='x', rotation=45)  # Rotate x-axis labels for better visibility
+axes[0, 1].set_ylabel('Time in minutes')
 
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 plt.show()
